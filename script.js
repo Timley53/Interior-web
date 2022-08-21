7d4e77573dea9113d8f9ea20617112223e49475a
Adedokun Timileyin <adedokuntimileyin123@gmail.com>
2022-08-21 04:26:38 -0700

    redo take 1

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 "strict mode";
+
 document.querySelector(".bar").addEventListener("click", function () {
-  document.querySelector(".fa-solid").classList.toggle("fa-bars");
-  document.querySelector(".fa-solid").classList.toggle("fa-xmark");
-  document.querySelector(".nav-bar").classList.toggle("display");
+  document.querySelector(".nav-bars").classList.toggle("display");
 });
+
+// document.querySelector(".bar").addEventListener("click", function () {
+//   document.querySelector(".fa-solid").classList.toggle("fa-bars");
+//   document.querySelector(".fa-solid").classList.toggle("fa-xmark");
+//   document.querySelector(".nav-bars").classList.toggle("display");
+// });
